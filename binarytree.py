6243ad6a89cca8ab6dc09b08d7653185a7516242
Karthik-Sankar <krthknsnkr@gmail.com>
2018-07-05 22:08:06 +0530

    Recent Changes in binary tree

--- binarytree.py
+++ binarytree.py
@@ -1,33 +1,56 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jun 12 20:19:31 2018
-
-@author: karthikeyan sankar
-"""
-
-#Binary tree
-#   tree
-#   ----
-#     1
-#    / \
-#   2   3
-#  /
-# 4
-# Program to create simp
-class Node:
-    def __init__(self,val):
-        self.value = val
+class Node(object):
+    def __init__(self, value):
+        self.value = value
+        self.left = None
         self.right = None
-        self.left  = None
-class BinaryTree:
-    def __init__(self,val):
-        self.root  = Node(val)
-    def insert(self, data):
-        temp = Node(data)
-                
-#root = Node(1)
-x = BinaryTree(1)
-x.root.left=Node(2)
-x.root.right=Node(3)
-x.root.left.left=Node(4)
+
+class BinaryTree(object):
+    def __init__(self, root):
+        self.root = Node(root)
+
+    def search(self, find_val):
+        """Return True if the value
+        is in the tree, return
+        False otherwise."""
+        return self.preorder_search(self.root, find_val)
+
+    def print_tree(self):
+        """Print out all tree nodes
+        as they are visited in
+        a pre-order traversal."""
+        return ""
+
+    def preorder_search(self, start, find_val):
+        """Helper method - use this to create a 
+        recursive search solution."""
+        if(start):
+        #    return False
+            if(start.value==find_val):
+                return True
+            else:
+                return self.preorder_search(start.left,find_val) or self.preorder_search(start.right,find_val)
+        else:
+            return False
+
+    def preorder_print(self, start, traversal):
+        """Helper method - use this to create a 
+        recursive print solution."""
+        return traversal
+
+
+# Set up tree
+tree = BinaryTree(1)
+tree.root.left = Node(2)
+tree.root.right = Node(3)
+tree.root.left.left = Node(4)
+tree.root.left.right = Node(5)
+#print(tree.root.value)
+# Test search
+# Should be True
+print(tree.search(4))
+# Should be False
+print(tree.search(6))
+
+# Test print_tree
+# Should be 1-2-4-5-3
+#print tree.print_tree()
